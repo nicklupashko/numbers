aed4744e347d127d6ab88a3d31297bf0d51517b2
Nick Lupashko <n.r.lupashko@gmail.com>
2017-05-25 08:44:37 +0300

    [Classification] Read data from file

--- src/main/scala/Classification.scala
+++ src/main/scala/Classification.scala
@@ -1,17 +1,17 @@
+import scala.io.Source
 import scala.util.Random
 
 object Classification {
-  val numberOfFeatures = 5
-  val numberOfElements = 10
+  val dataSet: Array[Array[Double]] = dataSetFromFile("data.txt")
 
-  val dataSet: Array[Array[Double]] = Array.ofDim[Double](numberOfFeatures, numberOfElements)
-  dataSet(0) = Array(1.23,   1.04,  1.8,    0.43,  0.88,  0.57,  1.72,   1.7,   0.84,  0.6)
-  dataSet(1) = Array(0.23,   0.39,  0.43,   0.18,  0.15,  0.34,  0.38,   0.09,  0.14,  0.21)
-  dataSet(2) = Array(1.45,   1.3,   1.37,   1.65,  1.91,  1.68,  1.94,   1.89,  1.94,  2.06)
-  dataSet(3) = Array(167.69, 186.1, 220.45, 169.3, 39.53, 40.41, 102.96, 37.02, 45.74, 40.07)
-  dataSet(4) = Array(17.72,  18.39, 26.46,  22.37, 28.13, 17.55, 21.92,  19.52, 23.99, 21.76)
+  val clusterList: List[String] =
+    (0 until dataSet.head.length).map(_.toString).toList
 
-  val clusterList: List[String] = (0 until numberOfElements).map(_.toString).toList
+  def dataSetFromFile(filePath: String): Array[Array[Double]] = {
+    val regex = """\d+(\.\d+)?""".r
+    Source.fromFile(filePath).getLines().toList
+      .map(regex.findAllIn(_).map(_.toDouble).toArray).toArray
+  }
 
   def distanceMatrix(arr: Array[Array[Double]]): Array[Array[Double]] = {
     val N: Int = arr.length
